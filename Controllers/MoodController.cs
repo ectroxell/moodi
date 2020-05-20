f142bfa410e648d4210dc6da226e2d88e41bfdd0
Emma Troxell <troxellemma@gmail.com>
2020-05-20 15:30:09 -0500

    started basic code

---
+++ Controllers/MoodController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using moodi.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace moodi.Controllers
+{
+    public class MoodController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        
+    }
+}
---
+++ Models/Mood.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace moodi.Models
+{
+    public class Mood
+    {
+        string Name { get; set; }
+        int ID { get; set; }
+    }
+}
